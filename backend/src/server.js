a21c4350be00d4477d201b352f8e8d4039164290
ataousCode <microservicestech97@gmail.com>
2025-07-01 02:56:27 +0800

    Refactor: Moved model files to models/ folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from 'cors';
+import {clerkMiddleware} from '@clerk/express';
+
 import { EVN } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.get("/api/", (req, res) => res.send("Hello connection"));
+
+app.use("/api/users", )
 
 const startServer = async () => {
   try {
