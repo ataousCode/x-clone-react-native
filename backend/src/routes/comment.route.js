d368bbb5568f33e98a9461385098f5c9b4353d3e
ataousCode <microservicestech97@gmail.com>
2025-07-02 18:04:47 +0800

    fix small issues

--- backend/src/routes/comment.route.js
+++ backend/src/routes/comment.route.js
@@ -1,8 +1,8 @@
-import exporess from 'express';
-import { createComment, deleteComment, getComments } from '../controllers/comment.controller';
-import { protectRoute } from '../middleware/auth.middleware';
+import express from 'express';
+import { createComment, deleteComment, getComments } from '../controllers/comment.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
-const router = exporess.Router();
+const router = express.Router();
 
 router.get('/post/:postId', getComments);
 
