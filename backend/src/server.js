5fb0f27986aaed7222ab75bcbd8fdcf19e0bbaf5
ataousCode <microservicestech97@gmail.com>
2025-07-02 18:26:15 +0800

    added controller functions and impl arject in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,13 +9,15 @@ import notificationRoutes from "./routes/notification.route.js";
 
 import { EVN } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get("/api/", (req, res) => res.send("Hello connection"));
 
@@ -43,3 +45,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+export default app;
